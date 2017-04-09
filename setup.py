b5b8d909d461bd8f4bb20148570161f1cbdd96c0
Jessica Glenn <jessica@skilld.com>
2017-04-09 22:47:48 +1000

    updating version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nephelo',
-      version='0.1',
+      version='0.101',
       description='A Cloud Formation library to more easily. Nephelo, short for Nephelococcygia, is the art of watching clouds.',
       url='https://github.com/tamme-io/nephelo',
       author='tamme',
